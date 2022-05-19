503e985cf0f05c6700ee33452dbbb91ba716b3c3
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-05-19 16:02:55 +0430

    Add container center and padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+      content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+      theme: {
+            extend: {},
+            container: {
+                  center: true,
+                  padding: "2rem",
+            },
+      },
+      plugins: [],
+};

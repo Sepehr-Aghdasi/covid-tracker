49bf64bc9802d76a61277d29d862e164229f4383
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-05-19 16:10:36 +0430

    Complete Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+      <header class="text-center bg-blue-900 text-white p-3 mb-10">
+            <div class="text-3xl md:text-4xl font-bold mb-3">
+                  <i class="fa fa-viruses"></i> Covid-19 Tracker
+            </div>
+      </header>
+</template>
 
 <script>
 export default {

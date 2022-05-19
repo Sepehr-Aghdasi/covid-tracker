328564cb9332a2698dfe7447a5c4837942c7e856
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-05-19 18:34:53 +0430

    Fetch data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,41 @@
 <template>
-      <div class="home">Hello World</div>
+      <main v-if="!loading">show data</main>
+      <main v-else class="flex flex-col align-center justify-center text-center">
+            <div class="text-gray-500 text-3xl mt-10 mb-6">
+                  <p>Fetching Data</p>
+            </div>
+            <img :src="loadingImage" class="w-80 m-auto" />
+      </main>
 </template>
 
 <script>
 export default {
       name: "HomeView",
       components: {},
+      data() {
+            return {
+                  loading: true,
+                  title: "Global",
+                  dataDate: "",
+                  stats: {},
+                  countries: [],
+                  loadingImage: require("../assets/Ripple-1s-200px.gif"),
+            };
+      },
+      methods: {
+            async fetchCovidData() {
+                  const response = await fetch("https://api.covid19api.com/summary");
+                  const data = await response.json();
+                  return data;
+            },
+      },
+      async created() {
+            const data = await this.fetchCovidData();
+            console.log(data);
+            this.dataDate = data.Date;
+            this.stats = data.Global;
+            this.countries = data.Countries;
+            this.loading = false;
+      },
 };
 </script>

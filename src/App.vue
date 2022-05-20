12146875c62bae3876af1ba51f3869506e878b5d
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-05-20 13:46:11 +0430

    Add CSS Reset

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+      margin: 0;
+      padding: 0;
+      outline: none;
+      text-decoration: none;
+      list-style-type: none;
+      box-sizing: border-box;
+      text-transform: capitalize !important;
+}
+</style>

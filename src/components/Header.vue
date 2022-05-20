24306a652249fdbb57cb14fd153a5b39956e52fa
Sepehr-Aghdasi82 <Sepehr.Aghdasi82@gmail.com>
2022-05-20 13:47:53 +0430

    Chnage Covid icon color to green

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
       <header class="text-center bg-blue-900 text-white p-3 mb-10">
             <div class="text-3xl md:text-4xl font-bold mb-3">
-                  <i class="fa fa-viruses"></i> Covid-19 Tracker
+                  <i class="fa fa-viruses text-green-500"></i> Covid-19 Tracker
             </div>
       </header>
 </template>
